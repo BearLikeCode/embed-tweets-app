385646349affc36dbdb454fd036353dbe220002c
Eugen <eilinskih92@gmail.com>
2022-08-04 15:31:25 +0300

    entireapp

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const {createServer} = require('http')
+const { Server } = require('socket.io')
+const port = process.env.REACT_APP_PORT || 3002
+const app = express()
+
+const server = createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+})
+let socketConnection
+
+io.on("connection", socket => {
+    if (socketConnection === undefined) {
+    socketConnection = socket
+    }
+    require('./routes/tweets.js')(app, socketConnection)
+})
+io.on("disconnect", () => server.close())
+
+server.listen(port, () => {
+    console.log('server is up')
+})
