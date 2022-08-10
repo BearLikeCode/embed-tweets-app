892079f58e140753cdcc1283a38b7da771f828c1
Eugen <eilinskih92@gmail.com>
2022-08-10 17:57:16 +0300

    new app

--- twits/src/App.jsx
+++ twits/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [tweets, setTweets] = useState([])
 
   useEffect(() => {
-    const socket = io.connect('http://localhost:3002/')
+    const socket = io.connect(process.env.PORT || 'http://localhost:3002/')
     socket.on('connect', () => {
       console.log('socket connect')
       socket.on('tweets', data => {
