2af3bfe6f9cd378759e5f5d051eb9ba5a6621ccf
Eugen Illinskih <eilinskih92@gmail.com>
2022-09-23 02:22:29 +0300

    todos

--- twits/src/App.jsx
+++ twits/src/App.jsx
@@ -56,7 +56,7 @@ function App() {
       const filters = `${query.filter(item => !item.includes('@')).length > 1 ? '(' : ''}${query.filter(item => !item.includes('@')).length > 0 ? query.filter(item => !item.includes('@')).map(hashtag => !hashtag.includes('#') ? `#${hashtag}` : hashtag).join(' OR ') : ''}${query.filter(item => !item.includes('@')).length > 1 ? ')' : ''}${query.filter(item => item.includes('@')).length > 0 && query.filter(item => !item.includes('@')) ? ' ' : ''}${query.filter(item => item.includes('@')).length > 1 ? '(' : ''}${query.filter(item => item.includes('@')).length > 0 ? (query.filter(item => item.includes('@')).join(' OR ').replaceAll('@','from:')) : ''}${query.filter(item => item.includes('@')).length > 1 ? ')' : ''}`
       setIsLoading(true)
       axios
-        .get('http://localhost:3002/api/recent', {
+        .get('/api/recent', {
           params: { filters }
         })
         .then((res) => {
