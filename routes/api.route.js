878a9b28530e4401a0fa3fc97ed7229e9e5a3c9d
Eugen Illinskih <eilinskih92@gmail.com>
2022-10-03 14:39:42 +0300

    auth connected

--- routes/api.route.js
+++ routes/api.route.js
@@ -59,7 +59,7 @@ router.get('/callback', (req, res, next) => {
   const { oauth_token, oauth_verifier } = req.query;
   // Get the saved oauth_token_secret from session
   const { oauth_token_secret } = req.session;
-res.send(oauth_token_secret)
+res.send(req.session)
 
   if (!oauth_token || !oauth_verifier || !oauth_token_secret) {
     return res.status(400).send('You denied the app or your session expired!');
