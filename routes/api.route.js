d5c954f131e295b2f4b2229ba8da33b9dd9a97aa
Eugen Illinskih <eilinskih92@gmail.com>
2022-10-06 02:18:27 +0300

    auth connected

--- routes/api.route.js
+++ routes/api.route.js
@@ -33,11 +33,12 @@ router.get('/recent-api', async (req, res, next) => {
     // console.log(recent.data)
     // res.send(recent.data)
     const tweetsList = { data: recent?.data?.data, includes: recent?.data?.includes, meta: recent?.data?.meta }
-    const newdoc = await Tweet.findOneAndUpdate({name: user.screen_name, id_str: user.id_str}, 
+    await Tweet.findOneAndUpdate({name: user.screen_name, id_str: user.id_str}, 
       {tweetsList}, 
       {upsert: true, new: true, setDefaultsOnInsert: true},
       (err, res) => {
         if (err) return
+        
       })
       res.send(newdoc)
   } catch (err) {
