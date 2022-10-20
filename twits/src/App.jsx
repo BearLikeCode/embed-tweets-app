4f1116eb5f7e0f0c0a98e803595a5a90a15ac257
Eugen Illinskih <eilinskih92@gmail.com>
2022-10-20 15:10:16 +0300

    auth connected

--- twits/src/App.jsx
+++ twits/src/App.jsx
@@ -185,10 +185,11 @@ function App() {
         window.clearInterval(i);
       }
 
-      const filters = `${query.filter(item => !item.includes('@')).length > 1 ? '(' : ''}${query.filter(item => !item.includes('@')).length > 0 ? query.filter(item => !item.includes('@')).map(hashtag => !hashtag.includes('#') ? `#${hashtag}` : hashtag).join(' OR ') : ''}${query.filter(item => !item.includes('@')).length > 1 ? ')' : ''}${query.filter(item => item.includes('@')).length > 0 && query.filter(item => !item.includes('@')) ? ' ' : ''}${query.filter(item => item.includes('@')).length > 1 ? '(' : ''}${query.filter(item => item.includes('@')).length > 0 ? (query.filter(item => item.includes('@')).join(' OR ').replaceAll('@', 'from:')) : ''}${query.filter(item => item.includes('@')).length > 1 ? ')' : ''}`
-      const amount = formValues.amount
       setIsLoading(true)
       const apiInt = setInterval(() => {
+        const filters = `${query.filter(item => !item.includes('@')).length > 1 ? '(' : ''}${query.filter(item => !item.includes('@')).length > 0 ? query.filter(item => !item.includes('@')).map(hashtag => !hashtag.includes('#') ? `#${hashtag}` : hashtag).join(' OR ') : ''}${query.filter(item => !item.includes('@')).length > 1 ? ')' : ''}${query.filter(item => item.includes('@')).length > 0 && query.filter(item => !item.includes('@')) ? ' ' : ''}${query.filter(item => item.includes('@')).length > 1 ? '(' : ''}${query.filter(item => item.includes('@')).length > 0 ? (query.filter(item => item.includes('@')).join(' OR ').replaceAll('@', 'from:')) : ''}${query.filter(item => item.includes('@')).length > 1 ? ')' : ''}`
+        const amount = formValues.amount
+  
       axios
         .get('/api/recent-api', {
           params: { filters, amount }
@@ -202,8 +203,8 @@ function App() {
         .catch((e) => {
           setIsLoading(false)
         })
+        setSearchParams({ filters })
       }, formValues.interval*60000)
-      setSearchParams({ filters })
       return () => clearInterval(apiInt)
     } else if (query.length === 0 && initialQuery !== null) {
       setQuery(initialQuery.split(' ').filter(query => query !== ' ' && query !== '#'))
