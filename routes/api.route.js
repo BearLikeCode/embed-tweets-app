c306a1aad423293d34b65ceb6a5d7c3fbbb329d4
Eugen Illinskih <eilinskih92@gmail.com>
2022-11-02 15:09:34 +0200

    auth connected

--- routes/api.route.js
+++ routes/api.route.js
@@ -99,7 +99,7 @@ router.get('/recent-api', async (req, res, next) => {
         if (value?.data?.data !== undefined) {
           dataArrs.push(value?.data?.data)
         }
-        console.log(value)
+        console.log(done)
         // initial.data = (value?.data?.data === undefined ? initial.data : initial.data.concat(value?.data?.data))
         initial.includes.media = (value?.data?.includes === undefined ? initial.includes.media : initial.includes.media.concat(value?.data?.includes?.media))
         initial.includes.users = (value?.data?.includes === undefined ? initial.includes.users : initial.includes.users.concat(value?.data?.includes?.users))
