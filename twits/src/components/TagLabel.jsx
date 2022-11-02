8d82707ab58d944e665a90e02afb54f6d5b9a439
Eugen Illinskih <eilinskih92@gmail.com>
2022-11-02 22:20:18 +0200

    auth connected

--- twits/src/components/TagLabel.jsx
+++ twits/src/components/TagLabel.jsx
@@ -4,7 +4,34 @@ import CloseIcon from '../assets/close.svg'
 const TagLabel = (props) => {
     const deleteHandler = (e) => {
         e.stopPropagation()
-        props.setSearchParams(prev => (prev.get('filters').split(' ').filter(item => !item.includes(props.tag)).length > 0 ? {filters: `${props.query.filter(tag => !tag.includes(props.tag)).filter(item => !item.includes('@')).length > 1 ? '(' : ''}${props.query.filter(item => !item.includes('@')).length > 0 ? props.query.filter(item => !item.includes('@')).map(hashtag => !hashtag.includes('#') ? `#${hashtag}` : hashtag).join(' OR ') : ''}${props.query.filter(item => !item.includes('@')).length > 1 ? ')' : ''}${props.query.filter(item => item.includes('@')).length > 0 && props.query.filter(item => !item.includes('@')) ? ' ' : ''}${props.query.filter(item => item.includes('@')).length > 1 ? '(' : ''}${props.query.filter(item => item.includes('@')).length > 0 ? (props.query.filter(item => item.includes('@')).join(' OR ').replaceAll('@', 'from:')) : ''}${props.query.filter(item => item.includes('@')).length > 1 ? ')' : ''}`} : {}))
+        props.setSearchParams(prev => (prev.get('filters')
+        .split(' ')
+        .filter(item => !item.includes(props.tag)).length > 0 && props.query.length > 0 ?
+        {filters: `${props.query
+          .filter(tag => !tag.includes(props.tag))
+          .filter(item => !item.includes('@')).length > 1 ? 
+          '(' : 
+          ''}${props.query
+            .filter(item => !item.includes('@')).length > 0 ? 
+            props.query
+            .filter(item => !item.includes('@'))
+            .map(hashtag => !hashtag.includes('#') ? 
+            `#${hashtag}` : hashtag).join(' OR ') : 
+            ''}${props.query
+              .filter(item => !item.includes('@')).length > 1 ? 
+              ')' : 
+              ''}${props.query.filter(item => item.includes('@')).length > 0 && 
+              props.query.filter(item => !item.includes('@')) ? 
+              ' ' : 
+              ''}${props.query.filter(item => item.includes('@')).length > 1 ? 
+              '(' : 
+              ''}${props.query.filter(item => item.includes('@')).length > 0 ? 
+              (props.query.filter(item => item.includes('@')).join(' OR ').replaceAll('@', 'from:')) : 
+              ''}${props.query.filter(item => item.includes('@')).length > 1 ? 
+              ')' : 
+              ''}`} :
+              {}
+          ))
         props.setQuery(prev => prev.filter(item => !item.includes(props.tag) && item !== ' ' && item !== '#'))
     }
   return (
